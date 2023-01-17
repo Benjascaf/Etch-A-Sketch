092dbe490eb9939e702c39c5f405420ce8a89717
benjascaf <benjascaf@gmail.com>
2023-01-17 20:42:55 -0300

    Add hover effect

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const container = document.querySelector("div");
-addDivLine(container);
+const container = document.querySelector(".container");
+fillContainer(container);
 
-function addDivLine(container) {
-    for (let j = 0; j < 16; j++) {
-        for (let i = 0; i < 16; i++) {
-            const miniDiv = document.createElement("div");
-            miniDiv.style.backgroundColor = "black";
-            container.appendChild(miniDiv);
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => square.addEventListener("mouseover",() => toggleColor(square)));
+
+function fillContainer(container, squaresPerSide = 16) {
+    for (let j = 0; j < squaresPerSide; j++) {
+        for (let i = 0; i < squaresPerSide; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.backgroundColor = "black";
+            container.appendChild(square);
         }
     }
 }
+
+function toggleColor(square) {
+    square.style.backgroundColor === "black" ? square.style.backgroundColor = "yellow"
+    : square.style.backgroundColor = "black";
+    }

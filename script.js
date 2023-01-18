4094c1586cfdb959246b5e35061ed0a05bcedf2a
benjascaf <benjascaf@gmail.com>
2023-01-18 18:07:08 -0300

    Add some style and buttons for user to change modes

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ function fillContainer(container, squaresPerSide = 16) {
             container.appendChild(square);
         }
     }
-    document.querySelector("body").appendChild(container);
+    document.querySelector(".border").appendChild(container);
     addButtonEventListeners();
 }
 
-function toggleColor(square) {
-    square.style.backgroundColor === "black" ? square.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`
-    : square.style.backgroundColor = "black";
+function toggleColor(square, mode="Default") {
+    switch (mode) {
+        case "Default":
+            square.style.backgroundColor = "yellow";
+            break;
+        case "Rainbow":
+            square.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+            break;
     }
+}
 
+function toggleMode(button) {
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+    addButtonEventListeners(button.textContent);
+    console.log(button.textContent);
+}
 function getUserLayout() {
     let layout = parseInt(prompt("How many rows and columns do you want to display? \n Type a number between 1-100", "16"));
     if (!layout || layout > 100 || layout < 1) {
@@ -45,17 +57,27 @@ function prepareGrid(container,layout) {
     container.style.gridTemplateColumns = `repeat(${layout}, 1fr)`;
 }
 
-function addButtonEventListeners() {
+function addButtonEventListeners(mode="Default") {
     const squares = document.querySelectorAll(".square");
-    squares.forEach(square => square.addEventListener("mouseover", () => toggleColor(square)));
+    squares.forEach(square => square.addEventListener("mouseover", () => toggleColor(square, mode)));
 }
 function getRandomColor() {
     return Math.floor(Math.random() * 255 + 1);
 }
 
+function clearGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.style.backgroundColor = "black");
+}
+
 const container = document.querySelector(".container");
 fillContainer(container);
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => fillContainer(resetContainer(), getUserLayout()));
+const gridButton = document.querySelector(".grid-btn");
+gridButton.addEventListener("click", () => fillContainer(resetContainer(), getUserLayout()));
 
+const modeSelectorButtons = document.querySelectorAll(".mode-selector");
+modeSelectorButtons.forEach(button => button.addEventListener("click", () => toggleMode(button)));
+
+const clearButton = document.querySelector(".clear-btn");
+clearButton.addEventListener("click", () => clearGrid());
